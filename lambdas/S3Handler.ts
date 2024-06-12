2c8d51f4d446c34eeb277a545c3759832e3692a6
sharan2717@gmail.com <sharan2717>
2024-06-12 17:26:03 +0530

    s

--- lambdas/S3Handler.ts
+++ lambdas/S3Handler.ts
@@ -1,106 +1,138 @@
-import * as AWS from 'aws-sdk';
-import {  PutItemInput, PutItemInputAttributeMap } from 'aws-sdk/clients/dynamodb';
-import { S3Event, S3Handler } from 'aws-lambda';
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { PutItemInputAttributeMap } from "aws-sdk/clients/dynamodb";
+import { S3Event, S3Handler } from "aws-lambda";
+import {
+  GetObjectCommand,
+  GetObjectCommandOutput,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { parse } from "csv-parse";
+import { Readable } from "stream";
+import { DynamoDBClient,BatchWriteItemCommand ,BatchWriteItemCommandInput } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 
-export const handler=async (event:S3Event)=>{
-    const client = new S3Client({});
+export const handler: S3Handler = async (event: S3Event) => {
+  const client = new S3Client({ region: "ap-south-1" });
 
-    const bucketName = event.Records[0].s3.bucket.name;
-    const s3ObjectKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-   let csvContent :string= ""
-    const command = new GetObjectCommand({
-        Bucket: bucketName,
-        Key:s3ObjectKey,
-      });
-    try {
-        const response = await client.send(command);
-        // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
-        if(response.Body){
-            const csvContent = await response.Body.transformToString();
-        }
-      } catch (err) {
-        console.error(err);
-      } 
+  const bucketName = event.Records[0].s3.bucket.name;
+  const s3ObjectKey = decodeURIComponent(
+    event.Records[0].s3.object.key.replace(/\+/g, " ")
+  );
+  let csvContent: string = "";
+  const command = new GetObjectCommand({
+    Bucket: bucketName,
+    Key: s3ObjectKey,
+  });
+  try {
+    const response: GetObjectCommandOutput = await client.send(command);
+    const csvStream = parse({ delimiter: "," });
+    if (response.Body instanceof Readable) {
+      console.log("here");
+      const chunks = [];
+      for await (const chunk of response.Body) {
+        chunks.push(chunk);
+      }
+      csvContent = Buffer.concat(chunks).toString("utf-8");
+    } else {
+      console.log("Response Body is not a Readable stream:", response.Body);
+    }
+  } catch (error) {
+    console.error("Error getting object from S3:", error);
+  }
 
-calculateresults(csvContent)
+  await calculateresults(csvContent);
 
-   return {
-    StatusCode : "200",
-    Body:{"Message" : "Results Calculated"}
-   };
-
-      
-}
+};
 
 const calculateGrade = (mark: number) => {
-    if (mark >= 90) return 'A+';
-    else if (mark >= 80) return 'A';
-    else if (mark >= 70) return 'B+';
-    else if (mark >= 60) return 'B';
-    else if (mark >= 40) return 'C';
-    else return 'D';
+  if (mark >= 90) return "A+";
+  else if (mark >= 80) return "A";
+  else if (mark >= 70) return "B+";
+  else if (mark >= 60) return "B";
+  else if (mark >= 40) return "C";
+  else return "D";
+};
+
+function calculateGPA(marks: any) {
+  const creditPoints: any = {
+    Tamil: 1,
+    English: 2,
+    Maths: 3,
+    Science: 2,
+    Social: 2,
   };
+  const gradeValues = { "A+": 10, A: 9, "B+": 8, B: 7, C: 6, D: 5 };
+  let totalCreditPoints = 0;
+  let totalGradePoints = 0;
 
+  for (const subject in marks) {
+    const grade = calculateGrade(marks[subject]);
+    const creditPoint = creditPoints[subject];
+    const gradeValue = gradeValues[grade];
 
+    totalCreditPoints += creditPoint;
+    totalGradePoints += gradeValue * creditPoint;
+  }
 
-  function calculateGPA(marks:any) {
-    const creditPoints:any = { 
-        Tamil: 1, 
-        English: 2, 
-        Maths: 3,
-         Science: 2,
-          Social: 2
-         };
-    const gradeValues = { 'A+': 10, 'A': 9, 'B+': 8, 'B': 7, 'C': 6, 'D': 5 };
-    let totalCreditPoints = 0;
-    let totalGradePoints = 0;
-
-    for (const subject in marks) {
-        const grade = calculateGrade(marks[subject]);
-        const creditPoint = creditPoints[subject];
-        const gradeValue = gradeValues[grade];
-
-        totalCreditPoints += creditPoint;
-        totalGradePoints += gradeValue * creditPoint;
-    }
-
-    return totalGradePoints / totalCreditPoints;
+  return totalGradePoints / totalCreditPoints;
 }
 
-
-
-async function calculateresults(csvContent:string){
-    const rows = csvContent.split('\n').slice(1);
-    const data = rows.map(row => {
-        const [Name, Tamil, English, Maths, Science, Social] = row.split(',').map(item => item.trim());
-        const GPA = calculateGPA({ Tamil: +Tamil, English: +English, Maths: +Maths, Science: +Science, Social: +Social });
-    
-        return {
-            Name,
-            Tamil: calculateGrade(+Tamil),
-            English: calculateGrade(+English),
-            Maths: calculateGrade(+Maths),
-            Science: calculateGrade(+Science),
-            Social: calculateGrade(+Social),
-            GPA: GPA.toFixed(2)
-        };
+async function calculateresults(csvContent: string) {
+  const rows = csvContent.split("\n").slice(1);
+  const data = rows
+  .map((row) => row.split(",").map((item) => item.trim())) 
+  .filter((items) => items[0]) 
+  .map(([Name, Tamil, English, Maths, Science, Social]) => {
+    const GPA = calculateGPA({
+      Tamil: +Tamil,
+      English: +English,
+      Maths: +Maths,
+      Science: +Science,
+      Social: +Social,
     });
-    await AWSaddDocument(data);
-   return data ;
+
+    return {
+      Name: Name,
+      Tamil: calculateGrade(+Tamil),
+      English: calculateGrade(+English),
+      Maths: calculateGrade(+Maths),
+      Science: calculateGrade(+Science),
+      Social: calculateGrade(+Social),
+      GPA: GPA.toFixed(2),
+    };
+  });
+  await AWSaddDocument(data);
+  return data;
 }
 
- async function AWSaddDocument(data:object){
-    const ddb = new AWS.DynamoDB.DocumentClient();
-    const params:PutItemInput = {
-      TableName: "Results",
-      Item:  data as PutItemInputAttributeMap,
+async function AWSaddDocument(data: any) {
+  const client = new DynamoDBClient({});
+  const docClient = DynamoDBDocumentClient.from(client);
+
+  const putItemCommands = data.map((item: any) => ({
+    PutRequest: {
+        Item: {
+            Name: { S: item.Name },
+            Tamil: { S: item.Tamil },
+            English: { S: item.English },
+            Maths: { S: item.Maths },
+            Science: { S: item.Science },
+            Social: { S: item.Social },
+            GPA: { S: item.GPA }
+          }   
+         }
+  }));
+  const params = {
+    RequestItems: {
+      'Results': putItemCommands 
+    }
   };
-     try{
-        await ddb.put(params).promise()
-        return params.Item;
-     }catch (error){
-      console.error('Error adding document:', error);
-      throw error;
+  const batchcommand = new BatchWriteItemCommand(params);
+  try {
+    console.log("here")
+    const response = await docClient.send(batchcommand);
+    console.log("here attr",response.ItemCollectionMetrics);
+  } catch (error) {
+    console.error("Error adding document:", error);
+    throw error;
   }
-  }
+}
--- lib/aws_training-stack.ts
+++ lib/aws_training-stack.ts
@@ -8,15 +8,9 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Asset } from 'aws-cdk-lib/aws-s3-assets';
-
-
-
 export class AwsTrainingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    
 const lambdaLayer = new lambda.LayerVersion(this, 'SampleLayer', {
   code: lambda.Code.fromAsset('layer'),
   compatibleRuntimes: [lambda.Runtime.NODEJS_18_X, lambda.Runtime.NODEJS_16_X],
@@ -39,7 +33,7 @@ const S3LambdaFunction = new NodejsFunction(this, "SampleFunction", {
   layers: [lambdaLayer],
   environment: {
     NODE_OPTIONS: '--experimental-modules' ,
-    TABLE_NAME: ResultsTable.tableName // Pass table name as environment variable
+    TABLE_NAME: ResultsTable.tableName 
 
   }
 }
@@ -51,13 +45,14 @@ ResultsTable.grantReadWriteData(S3LambdaFunction)
 const MarksCSVS3Bucket = new aws_s3.Bucket(this,"SampleBucket",{
   bucketName:"markscsv"
 })
+MarksCSVS3Bucket.grantRead(S3LambdaFunction);
+
 MarksCSVS3Bucket.addEventNotification(s3.EventType.OBJECT_CREATED,new s3n.LambdaDestination(S3LambdaFunction))
 
 
-const csvFilePath = "C:/Users/vsharan/Desktop/AWS/AWSTraining/marks/Marks.csv";
 
 new s3deploy.BucketDeployment(this, "DeployCsv", {
-  sources: [s3deploy.Source.asset(csvFilePath)],
+  sources: [s3deploy.Source.asset("C:/Users/vsharan/Desktop/AWS/AWSTraining/marks")],
   destinationBucket: MarksCSVS3Bucket
 });
 
